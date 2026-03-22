84cd4724cda531b58a9a14f199d0c02bee45f5ba
Pramit Manna <pramitmanna19@gmail.com>
2026-03-22 17:08:49 +0530

    code: stripe session route add

--- apps/api/modules/paymentIntegration/payment.route.ts
+++ apps/api/modules/paymentIntegration/payment.route.ts
@@ -3,6 +3,7 @@ import { getPaymentUrl } from "./payment.controller";
 import requireAuth from "../../shared/middleware/requireAuth";
 const router = express.Router();
 
-router.post("/create-payment-intent", requireAuth, getPaymentUrl);
+router.post("/createCheckoutSession", requireAuth, getPaymentUrl);
+
 
 export default router;
