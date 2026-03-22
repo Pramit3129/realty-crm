1eb0e275c2c2d77adb44286fd0b6ba14a2108c17
Pramit Manna <pramitmanna19@gmail.com>
2026-03-22 18:49:19 +0530

    fix: secret key handle safely

--- apps/api/modules/paymentIntegration/payment.controller.ts
+++ apps/api/modules/paymentIntegration/payment.controller.ts
@@ -1,8 +1,6 @@
 import type { Request, Response } from "express";
-import stripe from "stripe";
 import type { AuthenticatedRequest } from "../../shared/middleware/requireAuth";
 import { PaymentService } from "./payment.service";
-const Stripe = new stripe(process.env.STRIPE_SECRET_KEY!);
 
 export const getPaymentUrl = async (
      req: Request,
@@ -11,7 +9,7 @@ export const getPaymentUrl = async (
      try {
           const authUser = req as AuthenticatedRequest;
           const { priceId } = req.body;
-          const url = await PaymentService.createSessionURL( authUser.user.id, priceId);
+          const url = await PaymentService.createSessionURL(authUser.user.id, priceId);
           res.json({ url: url });
      } catch (error: any) {
           res.status(500).json({ success: false, error: error.message });
--- apps/api/modules/paymentIntegration/payment.service.ts
+++ apps/api/modules/paymentIntegration/payment.service.ts
@@ -1,36 +1,45 @@
 import stripe from 'stripe';
 import {Subscription} from './subscription.model';
 export class PaymentService {
-     static Stripe = new stripe(process.env.STRIPE_SECRET_KEY!);
+     static SECRET_KEY: string;
+     static Stripe: stripe;
 
-     static async createSessionURL(userId: string, priceId: string){
-        const FRONTEND_URL : string = process.env.FRONTEND_URL!;
-        const  Plan = await Subscription.findOne({priceId});
-        if(!Plan){
-                throw new Error("Plan not found");
-        }
-        const PlanName:string = Plan.planName;     
-        if(!PlanName){
-                throw new Error("Plan name not found");
-        }
-        const session = await this.Stripe.checkout.sessions.create({
-                  mode: "subscription",
-                //   payment_method_types: ["card"],
-                  line_items: [
-                       {
-                            price: priceId,
-                            quantity: 1,
-                       },
-                  ],
-                  client_reference_id: userId,
-                  metadata: {
-                       userId: userId,
-                       planType: PlanName,
-                  },
-                  success_url: `${FRONTEND_URL}/success`,
-                  cancel_url: `${FRONTEND_URL}/cancel`,
-             });
+     static {
+          if (!process.env.STRIPE_SECRET_KEY) {
+               throw new Error("Stripe secret key not found in environment variables");
+          }
+          PaymentService.SECRET_KEY = process.env.STRIPE_SECRET_KEY;
+          PaymentService.Stripe = new stripe(PaymentService.SECRET_KEY);
+     }
 
-             return session.url;
+     static async createSessionURL(userId: string, priceId: string) {
+          const FRONTEND_URL: string = process.env.FRONTEND_URL!;
+          const Plan = await Subscription.findOne({ priceId });
+          if (!Plan) {
+               throw new Error("Plan not found");
+          }
+          const PlanName: string = Plan.planName;
+          if (!PlanName) {
+               throw new Error("Plan name not found");
+          }
+          const session = await this.Stripe.checkout.sessions.create({
+               mode: "subscription",
+               //   payment_method_types: ["card"],
+               line_items: [
+                    {
+                         price: priceId,
+                         quantity: 1,
+                    },
+               ],
+               client_reference_id: userId,
+               metadata: {
+                    userId: userId,
+                    planType: PlanName,
+               },
+               success_url: `${FRONTEND_URL}/success`,
+               cancel_url: `${FRONTEND_URL}/cancel`,
+          });
+
+          return session.url;
      }
 }
